1b95ae4f2cd215254fd61d6ddcf2f8e6bd3fccc9
theCount <thescheremetev@mail.ru>
2021-10-19 08:19:39 +0300

    Create new project for lern library ANT dising

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './app.sass'
+import Auth from "../Auth/Auth";
+
+function App() {
+  return (
+    <div className="App">
+      <Auth/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Auth/Auth.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './animation.css'
+import './auth.sass'
+
+const Auth = () => {
+    return(
+        <div className="Auth">
+            <div className="box box10"></div>
+            <div className="box box9"></div>
+            <div className="box box8"></div>
+            <div className="box box7"></div>
+            <div className="box box6"></div>
+            <div className="box box5"></div>
+            <div className="box box4"></div>
+            <div className="box box3"></div>
+            <div className="box box2"></div>
+            <div className="box box1"></div>
+            <div className="box box0"></div>
+        </div>
+    )
+}
+
+export default Auth
